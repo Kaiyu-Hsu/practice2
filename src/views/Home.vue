39a4d98aacffc12643a49621af694cb5d5d70119
Kaiyu <anne550323@gmail.com>
2021-09-12 02:04:44 +0800

    del: 不重要的

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Popular />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Popular from "./../components/popular"
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Popular,
   },
 };
 </script>
