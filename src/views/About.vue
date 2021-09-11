dcd90681eb6f553b6f3db2bf81c513183afc45ff
Kaiyu <anne550323@gmail.com>
2021-09-12 01:23:19 +0800

    add: popular but no api

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+      <!-- popular -->
+      <Popular />
+    </div>
 </template>
+
+<script>
+import Popular from '../components/popular.vue'
+export default {
+  name: 'About',
+  components: {
+    Popular
+  }
+}
+</script>
