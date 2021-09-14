1bcccd23769c19ad8c81b6fe0860694edd785753
Kaiyu <anne550323@gmail.com>
2021-09-14 17:07:34 +0800

    add: style

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <Navbar />
-    <Popular />
+    <div class="setting">
+      <header>帳戶設定</header>
+      <div class="form-container">
+        <form action="" id="profile-form"></form>
+      </div>
+    </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.setting {
+  position: absolute;
+  left: 378px;
+  height: 100%;
+  border-left: 1px solid #e6ecf0;
+}
+</style>
+
 <script>
-import Popular from "./../components/popular";
 import Navbar from "./../components/Navbar";
 
 export default {
   name: "Setting",
   components: {
-    Popular,
     Navbar,
   },
 };
