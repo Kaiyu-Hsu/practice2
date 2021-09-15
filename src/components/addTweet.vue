c83987de1490b1080db28855f239a5f32e50904f
Kaiyu <anne550323@gmail.com>
2021-09-15 11:30:56 +0800

    add: complete modal function add: tweet.JSON

--- src/components/addTweet.vue
+++ src/components/addTweet.vue
@@ -1,43 +1,49 @@
 <template>
-  <div class="modal-backdrop">
-    <div class="modal">
-      <div class="modal-header">
-        <button type="button" class="btn-close" @click="close">
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9.41387 7.99988L15.2069 2.20687C15.5969 1.81687 15.5969 1.18388 15.2069 0.792875C14.8169 0.401875 14.1839 0.402875 13.7929 0.792875L7.99988 6.58588L2.20687 0.792875C1.81687 0.402875 1.18388 0.402875 0.792875 0.792875C0.401875 1.18288 0.402875 1.81587 0.792875 2.20687L6.58588 7.99988L0.792875 13.7929C0.402875 14.1829 0.402875 14.8159 0.792875 15.2069C0.987875 15.4019 1.24287 15.4999 1.49987 15.4999C1.75687 15.4999 2.01187 15.4019 2.20687 15.2069L7.99988 9.41387L13.7929 15.2069C13.9879 15.4019 14.2429 15.4999 14.4999 15.4999C14.7569 15.4999 15.0119 15.4019 15.2069 15.2069C15.5969 14.8169 15.5969 14.1839 15.2069 13.7929L9.41387 7.99988Z"
-              fill="#FF6600"
-            />
-          </svg>
-        </button>
-        <slot name="header"></slot>
-      </div>
+  <!-- 轉場 -->
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal">
+        <div class="modal-header">
+          <button type="button" class="btn-close" @click="close">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.41387 7.99988L15.2069 2.20687C15.5969 1.81687 15.5969 1.18388 15.2069 0.792875C14.8169 0.401875 14.1839 0.402875 13.7929 0.792875L7.99988 6.58588L2.20687 0.792875C1.81687 0.402875 1.18388 0.402875 0.792875 0.792875C0.401875 1.18288 0.402875 1.81587 0.792875 2.20687L6.58588 7.99988L0.792875 13.7929C0.402875 14.1829 0.402875 14.8159 0.792875 15.2069C0.987875 15.4019 1.24287 15.4999 1.49987 15.4999C1.75687 15.4999 2.01187 15.4019 2.20687 15.2069L7.99988 9.41387L13.7929 15.2069C13.9879 15.4019 14.2429 15.4999 14.4999 15.4999C14.7569 15.4999 15.0119 15.4019 15.2069 15.2069C15.5969 14.8169 15.5969 14.1839 15.2069 13.7929L9.41387 7.99988Z"
+                fill="#FF6600"
+              />
+            </svg>
+          </button>
+          <!-- <slot name="header"></slot> -->
+        </div>
 
-      <div class="modal-body">
-        <slot name="body">
-          <img
-            src="https://www.meme-arsenal.com/memes/7ebba8a69f65afdf9f1f9f5ea7f3169f.jpg"
-          />
-        </slot>
-      </div>
+        <div class="modal-body">
+          <!-- <slot name="body"> -->
+          <img :src="user.avatar" />
+          <textarea
+            rows="4"
+            cols="50"
+            maxlength="140"
+            placeholder="有什麼新鮮事?"
+            v-model="tweet"
+          ></textarea>
+          <!-- </slot> -->
+        </div>
 
-      <footer class="modal-footer">
-        <!-- <slot name="footer">This is the default footer!</slot> -->
-        <button type="button" class="btn-green" @click="close">
-          Close Modal
-        </button>
-      </footer>
+        <footer class="modal-footer">
+          <!-- <slot name="footer">This is the default footer!</slot> -->
+          <button type="button" class="btn-tweet" @click="close">推文</button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .modal-backdrop {
   position: fixed;
   top: 0;
@@ -60,10 +66,11 @@
   overflow-x: auto;
   display: flex;
   flex-direction: column;
+  font-family: "Noto Sans TC", sans-serif;
+  font-weight: 500;
 }
 
-.modal-header,
-.modal-footer {
+.modal-header {
   padding: 27px;
   display: flex;
 }
@@ -76,12 +83,13 @@
 }
 
 .modal-footer {
-  border-top: 1px solid red;
+  /* border-top: 1px solid red; */
   flex-direction: column;
   justify-content: flex-end;
 }
 
 .modal-body {
+  display: flex;
   position: relative;
   padding: 15px;
 }
@@ -92,30 +100,71 @@ img {
   border-radius: 50%;
 }
 
+textarea {
+  padding: 0px;
+  margin: 10px;
+  resize: none;
+  width: 500px;
+  height: 100px;
+  font-size: 18px;
+  border: 0;
+  outline: none;
+  ::placeholder {
+    color: #9197a3;
+  }
+}
+
 .btn-close {
   position: absolute;
   left: 19.5px;
   top: 19.5px;
+  padding: 0px;
   border: none;
   font-size: 15px;
   cursor: pointer;
   background: transparent;
 }
 
-.btn-green {
+.btn-tweet {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 66px;
+  height: 38px;
+  position: relative;
+  left: 519px;
+  top: 39px;
+  background: #ff6600;
+  border: 1px solid #ff6600;
+  border-radius: 100px;
   color: white;
-  background: #4aae9b;
-  border: 1px solid #4aae9b;
-  border-radius: 2px;
+  font-size: 18px;
 }
 </style>
 
 <script>
+import tweetsJSON from "./../../public/tweets.json";
+
+const currentUser = tweetsJSON.userData;
 export default {
+  data() {
+    return {
+      user: {},
+      tweet: "",
+    };
+  },
   methods: {
+    fetchUser() {
+      this.user = currentUser;
+    },
     close() {
       this.$emit("close");
+      console.log(this.tweet);
     },
+  },
+  created() {
+    this.fetchUser();
   },
 };
 </script>
